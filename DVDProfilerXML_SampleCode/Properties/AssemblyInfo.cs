8a26723f89d643b6f65124a4f1c41d99d1f86945
DJ Doena <karsten@yajirobo.de>
2017-04-24 19:05:14 +0200

    initial

---
+++ DVDProfilerXML_SampleCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DVDProfilerXML_SampleCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Doena Soft.")]
+[assembly: AssemblyProduct("DVDProfilerXML_SampleCode")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2010 - 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
